9eaf2c4a2df5cad7eefc72dfcf47f7f829ef84c5
Pam Gaiguer <pamella.gaiguer@gmail.com>
2015-10-05 23:39:09 -0300

    iniciando site novo responsivo

---
+++ novo/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=emulateIE10">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Focco Câmbio - Câmbio e Turismo</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+  <header>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 blue-line">
+          texto header azul
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 gold-line">
+          texto header gold
+        </div>
+      </div>
+    </div>
+  </header>
+
+
+  <script src="js/jquery-2.1.3.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
