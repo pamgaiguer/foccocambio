ee46614d97185aa91aec8985336444cc8b1cf03d
Pam Gaiguer <pamella.gaiguer@gmail.com>
2015-10-07 20:01:49 -0300

    topo okay e mobile tbm

--- novo/index.php
+++ novo/index.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Focco Câmbio - Câmbio e Turismo</title>
   <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/font-avenir.css">
   <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
@@ -23,18 +24,17 @@
     <div class="container">
       <div class="row">
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-          <p><span>Delivery: </span> 11 5051-5299</p>
+          <p class="top-phone"><span>Delivery: </span> 11 5051-5299</p>
         </div>
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-          <h1>logotipo</h1>
+          <h1><img src="images/logo_topo.png" height="53" width="264" alt=""></h1>
         </div>
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
           <p><a href="http://www.facebook.com/focco.cambioeturismo">face.com/focco.cambioeturismo</a><i class="hidden-xs"><img src="images/logo_fb.jpg"></i></p>
         </div>
       </div>
     </div>
-  </header>
-  <nav class="navbar navbar-default">
+  <nav class="navbar">
     <div class="container">
       <div class="navbar-header">
         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -44,23 +44,23 @@
           <span class="icon-bar"></span>
         </button>
       </div>
-
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav nav-pills nav-justified">
-        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-          <li><a href="#">O que fazemos</a></li>
+          <li><a href="#">O que fazemos <span class="sr-only">(current)</span></a></li>
           <li><a href="#">Delivery</a></li>
           <li><a href="#">Produtos e serviços</a></li>
           <li><a href="#">Seguro Viagem</a></li>
           <li><a href="#">Contato</a></li>
         </ul>
-
       </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
   </nav>
+  </header>
+
 
 
   <script src="js/jquery-2.1.3.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script>
 </body>
 </html>
