1c6d70d6ed50958902170b260c545b1c467391ab
Pam Gaiguer <pamella.gaiguer@gmail.com>
2015-10-07 22:42:02 -0300

    footer add

--- novo/index.php
+++ novo/index.php
@@ -62,28 +62,81 @@
       </div><!-- /.container-fluid -->
     </nav>
   </header>
-  <div class="container-fluid">
-    <div id="slideshow" class="carousel slide" data-ride="carousel">
-      <div class="carousel-inner" role="listbox">
-        <div class="item active">
-          <img src="images/slide1.jpg" alt="focco1" class="img-responsive center-block">
+
+  <section class="slide">
+    <div class="container-fluid">
+      <div id="slideshow" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner" role="listbox">
+          <div class="item active">
+            <img src="images/slide1.jpg" alt="focco1" class="img-responsive center-block">
+          </div>
+          <div class="item">
+            <img src="images/slide2.jpg" alt="focco2" class="img-responsive center-block">
+          </div>
         </div>
-        <div class="item">
-          <img src="images/slide2.jpg" alt="focco2" class="img-responsive center-block">
+        <ol class="carousel-indicators">
+          <li data-target="#slideshow" data-slide-to="0" class="active"></li>
+          <li data-target="#slideshow" data-slide-to="1"></li>
+        </ol>
+      </div>
+    </div>
+  </section>
+
+  <section class="delivery"></section>
+  <section class="products-services"></section>
+  <section class="travel-insurance"></section>
+  <section class="contact-us"></section>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 blue-line">
+        texto header azul
+      </div>
+    </div>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+            <div class="media">
+              <div class="media-left media-middle">
+                <a href="https://www.facebook.com/focco.cambioeturismo" target="_blank">
+                  <img class="media-object" src="images/logo_fb_footer.png" alt="facebook">
+                </a>
+              </div>
+              <div class="media-body">
+                <p class="media-heading">
+                  Acompanhe nossa fanpage e fique por dentro de dicas e novidades de viagens
+                </p>
+              </div>
+              <a href="https://www.facebook.com/focco.cambioeturismo" target="_blank">facebook/focco.cambioeturismo</a>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+            <ul class="list-unstyled">
+              <li>O que fazemos</li>
+              <li>Delivery</li>
+              <li>Produtos e Serviços</li>
+              <li>Seguro Viagem</li>
+              <li>Contato</li>
+            </ul>
+          </div>
+          <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+            <ul class="list-unstyled">
+              <li>Contato</li>
+              <li>11 5051 5299</li>
+              <li><a href="mailto:contato@foccocambio.com.br">contato@foccocambio.com.br</a></li>
+              <li>R. Carlos Vicari, 155 - Pompeia/SP</li>
+            </ul>
+          </div>
+          <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+            <img src="images/logo_footer.png" alt="">
+          </div>
+
         </div>
       </div>
-      <ol class="carousel-indicators">
-        <li data-target="#slideshow" data-slide-to="0" class="active"></li>
-        <li data-target="#slideshow" data-slide-to="1"></li>
-      </ol>
-    </div>
-  </div>
-
-
-
-
-  <script src="js/jquery-2.1.3.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/main.js"></script>
-</body>
-</html>
+    </footer>
+    <script src="js/jquery-2.1.3.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+  </html>
