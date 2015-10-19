bc7a070e2e1fff73920f351400e6256bfd070e8d
Pamella Gaiguer <pamella.gaiguer@gmail.com>
2015-10-19 13:35:14 -0200

    navbar fixed to top

--- novo/index.php
+++ novo/index.php
@@ -16,7 +16,7 @@
   <link rel="shortcut icon" href="../favicon.ico" />
 </head>
 <body>
-  <header>
+  <header class="navbar-fixed-top">
     <div class="container-fluid">
       <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 blue-line">
