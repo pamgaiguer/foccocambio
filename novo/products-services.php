e839451f281ad15dd84a5eedf61482999a81e881
Pam Gaiguer <pamella.gaiguer@gmail.com>
2015-10-21 20:25:58 -0200

    pagina de produtos e serviços pronta para aprovação

--- novo/products-services.php
+++ novo/products-services.php
@@ -1,50 +1,141 @@
 <?php include '_header.php';?>
-<div class="container">
-
-<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-  <div class="panel panel-default">
-    <div class="panel-heading" role="tab" id="headingOne">
-      <h4 class="panel-title">
-        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          <img src="images/icon_wester.png" height="119" width="177" alt=""><span>Western Union</span>
-        </a>
-      </h4>
-    </div>
-    <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
-      <div class="panel-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-  <div class="panel panel-default">
-    <div class="panel-heading" role="tab" id="headingTwo">
-      <h4 class="panel-title">
-        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          <img src="images/icon_corp.png" height="119" width="177" alt=""><span>Atendimento Corporativo</span>
-        </a>
-      </h4>
-    </div>
-    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
-      <div class="panel-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-  <div class="panel panel-default">
-    <div class="panel-heading" role="tab" id="headingThree">
-      <h4 class="panel-title">
-        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          <img src="images/icon_seguro.png" height="119" width="177" alt=""><span>Seguro Viagem</span>
-        </a>
-      </h4>
-    </div>
-    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
-      <div class="panel-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
+<div class="container-fluid">
+  <img src="images/banner_int_produtos_servicos.jpg" class="img-responsive" alt="Produtos e Serviços" title="Produtos e Serviços">
 </div>
+
+<div class="container">
+  <ol class="breadcrumb visible-sm visible-md visible-lg hidden-xs">
+    <li><a href="#">Home</a></li>
+    <li class="active">Produtos e Serviços</li>
+  </ol>
+  <div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+      <h1 class="text-center blue-text"><strong>Produtos e Serviços</strong></h1>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+
+      <div class="media">
+        <div class="media-left media-middle">
+          <a href="int_moeda.php">
+            <img class="media-object" src="images/icon_money_circle.png" class="img-responsive">
+          </a>
+        </div>
+        <div class="media-body">
+          <h4 class="media-heading">Compra e venda de moeda estrangeira</h4>
+          <p>Melhores taxas do mercado, cédulas novas, <br>atendimento diferenciado por uma equipe treinada.</p>
+          <a href="int_moeda.php">Saiba Mais</a>
+        </div>
+      </div>
+
+      <div class="media">
+        <div class="media-left media-middle">
+          <a href="int_transf_internacional.php">
+            <img class="media-object" src="images/icon_transfer_circle.png" class="img-responsive">
+          </a>
+        </div>
+        <div class="media-body">
+          <h4 class="media-heading">Transferência Internacional</h4>
+          <p>
+            Manutenção de residentes / cursos e congressos / transferências de patrimônio e disponibilidade /
+            Compra e venda de imóveis / importação e exportação com total segurança.<br>
+            Equipe capacitada com profissionais com ampla experiências no mercado financeiro para indicar a melhor opção.
+          </p>
+          <a href="int_transf_internacional.php">Saiba Mais</a>
+        </div>
+      </div>
+
+      <div class="media">
+        <div class="media-left media-middle">
+          <a href="travel-insurance.php">
+            <img class="media-object" src="images/icon_seguro_circle.png" class="img-responsive">
+          </a>
+        </div>
+        <div class="media-body">
+          <h4 class="media-heading">Seguro Viagem</h4>
+          <p>
+            Somos parceiros FLYCARD, o único grupo que oferece cobertura emergencial para doenças pré-existentes,
+            seguro bagagem inclusive em trechos internos de sua viagem internacional, não é por reembolso e dispõe
+            de uma central de atendimento 24h em português, além de outros benefícios e cliente FOCCO tem desconto!<br>
+            Consulte-nos!
+          </p>
+          <a href="travel-insurance.php">Saiba Mais</a>
+        </div>
+      </div>
+
+      <div class="media">
+        <div class="media-left media-middle">
+          <a href="int_atend_corporativo.php">
+            <img class="media-object" src="images/icon_corporativo_circle.png" class="img-responsive">
+          </a>
+        </div>
+        <div class="media-body">
+          <h4 class="media-heading">Atendimento corporativo</h4>
+          <p>
+            Melhores condições e taxas para atender as necessidades de funcionários viajantes, pagamentos internacionais,
+            importação/exportação etc. Sempre com a ajuda de um executivo de contas exclusivo.
+          </p>
+          <a href="int_atend_corporativo.php">Saiba Mais</a>
+        </div>
+      </div>
+
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+
+      <div class="media">
+        <div class="media-left media-middle">
+          <a href="int_cartao_prepago.php">
+            <img class="media-object" src="images/icon_cartao_circle.png" class="img-responsive">
+          </a>
+        </div>
+        <div class="media-body">
+          <h4 class="media-heading">Cartão Visa Travel Money</h4>
+          <p>
+            Seguro, prático, nominal e recarregável, emitido sem custo nenhum e com a abrangência <em>VISA</em> presente em mais de 200 países.
+          </p>
+          <a href="int_cartao_prepago.php">Saiba Mais</a>
+        </div>
+      </div>
+
+      <div class="media">
+        <div class="media-left media-middle">
+          <a href="delivery.php">
+            <img class="media-object" src="images/icon_delivery_circle.png" class="img-responsive">
+          </a>
+        </div>
+        <div class="media-body">
+          <h4 class="media-heading">Delivery</h4>
+          <p>
+            Receba em seu endereço comercial ou residencial, sempre no dia útil seguinte a sua solicitação, com total <br>
+            descrição e segurança feito por portadores de nossa empresa e sem custo nenhum.<br>
+            Consulte-nos para valor mínimo e regiões atendidas.
+          </p>
+          <a href="delivery.php">Saiba Mais</a>
+        </div>
+      </div>
+
+      <div class="media">
+        <div class="media-left media-middle">
+          <a href="int_western_union.php">
+            <img class="media-object" src="images/icon_wu_circle.png" class="img-responsive">
+          </a>
+        </div>
+        <div class="media-body">
+          <h4 class="media-heading">Western Union</h4>
+          <p>
+            Somos um agente autorizado - Nunca foi tão fácil enviar e receber dinheiro, sem burocracia e de forma rápida e segura.
+            (Manutenção de residente)
+          </p>
+          <a href="int_western_union.php">Saiba Mais</a>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+
 </div>
 
 <?php include '_footer.php';?>
