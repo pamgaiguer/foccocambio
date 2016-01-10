91d23661bd6a7ea159ccc429352f1bfe50d0a7cc
Pamella Gaiguer <pamella.gaiguer@gmail.com>
2016-01-10 15:57:24 -0200

    pagina cartao pre pago finalizada

---
+++ novo/comp_prepaid_card.php
@@ -0,0 +1,222 @@
+<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="prepaid-saques">
+      <a role="button" data-toggle="collapse" data-parent="#accordion" href="#prepaidcard-saques" aria-expanded="true" aria-controls="prepaidcard-saques">
+        <h4 class="panel-title">
+          Como realizar saques e compras?
+        </h4>
+      </a>
+    </div>
+    <div id="prepaidcard-saques" class="panel-collapse collapse" role="tabpanel" aria-labelledby="prepaid-saques">
+      <div class="panel-body">
+        <p>Para compras em estabelecimentos credenciados:</p>
+        <p>Basta passar o cartão e digitar sua senha ou assinar o comprovante de compra no estabelecimento credenciado Visa. Por se tratar de um cartão pré-pago, em alguns países a transação será passada na
+         função débito e em outros na função crédito, porém, todas as transações serão sempre debitadas automaticamente do saldo disponível.</p>
+       </div>
+     </div>
+   </div>
+
+   <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="prepaid-internet">
+      <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#prepaidcard-internet" aria-expanded="false" aria-controls="prepaidcard-internet">
+        <h4 class="panel-title">
+          Para compras na Internet:
+        </h4>
+      </a>
+    </div>
+    <div id="prepaidcard-internet" class="panel-collapse collapse" role="tabpanel" aria-labelledby="prepaid-internet">
+      <div class="panel-body">
+        <p>Basta digitar, no site de compras, os dados que poderão ser solicitados:</p>
+        <ul>
+          <li>Número do cartão (16 dígitos localizados na frente do cartão);</li>
+          <li>Validade do cartão (impressa na frente do cartão - MM/AA);</li>
+          <li>Código de segurança (3 dígitos impressos no verso do cartão, próximos à tarja magnética).</li>
+          <li>Nome completo do portador*;</li>
+          <li>Endereço completo.</li>
+          <p>
+            <small>
+              Aceitação do cartão pelo site na internet pode variar de acordo com a loja virtual, por isso, algumas transações podem ser recusadas.
+              Transações realizadas em moeda diferente da moeda do seu cartão serão convertidas seguindo as taxas de conversão da Visa.
+              As transações serão autorizadas somente se houver saldo disponível no cartão.
+            </small>
+          </p>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="prepaid-looting">
+      <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#prepaidcard-looting" aria-expanded="false" aria-controls="prepaidcard-looting">
+        <h4 class="panel-title">
+          Para Saques:
+        </h4>
+      </a>
+    </div>
+    <div id="prepaidcard-looting" class="panel-collapse collapse" role="tabpanel" aria-labelledby="prepaid-looting">
+      <div class="panel-body">
+        <p>
+          Insira o cartão em um caixa automático com selo da rede Plus, escolha a função crédito ou “não correntista”, digite a sua senha e siga as demais instruções.
+        </p>
+        <p>
+         Verificar sempre o limite máximo permitido em cada caixa automático, que pode varias de acordo com o local e horário. Para cada saque efetuado, o valor de 2,50 na moeda do cartão é debitado automaticamente do seu cartão.</p>
+       </div>
+     </div>
+   </div>
+
+   <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="prepaid-debts">
+      <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#prepaidcard-debts" aria-expanded="false" aria-controls="prepaidcard-debts">
+        <h4 class="panel-title">
+          Como são efetuados os débitos referentes a saques e compras?
+        </h4>
+      </a>
+    </div>
+    <div id="prepaidcard-debts" class="panel-collapse collapse" role="tabpanel" aria-labelledby="prepaid-debts">
+      <div class="panel-body">
+        <p>Todas as transações são debitadas na moeda do cartão (dólar americano, euro, libra esterlina, dólar australiano, dólar canadense ou peso argentino).</p>
+        <p>Transações realizadas onde a moeda local é diferente da moeda do seu cartão serão convertidas seguindo as taxas de conversão da Visa. </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="prepaid-refills">
+      <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#prepaidcard-refills" aria-expanded="false" aria-controls="prepaidcard-refills">
+        <h4 class="panel-title">
+          Recargas
+        </h4>
+      </a>
+    </div>
+    <div id="prepaidcard-refills" class="panel-collapse collapse" role="tabpanel" aria-labelledby="prepaid-refills">
+      <div class="panel-body">
+        <p>Basta entrar em contato com a Focco Câmbio pelo telefone <a href="tel:+551150515299">(11) 5051-5299</a>, através do e-mail <a href="mailto:contato@foccocambio.com.br">contato@foccocambio.com.br</a> ou comparecer pessoalmente em qualquer uma de nossas lojas*</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="prepaid-return">
+      <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#prepaidcard-return" aria-expanded="false" aria-controls="prepaidcard-return">
+        <h4 class="panel-title">
+          O que você pode fazer com seu cartão ao voltar para o Brasil:
+        </h4>
+      </a>
+    </div>
+    <div id="prepaidcard-return" class="panel-collapse collapse" role="tabpanel" aria-labelledby="prepaid-return">
+      <div class="panel-body">
+        <p>Na existência de saldo disponível no cartão ao retornar ao Brasil, você poderá guardá-lo para uma próxima viagem, resgatá-lo ou adicionar a ele o valor que desejar. Essas operações poderão ser realizadas pela Focco.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="prepaid-saldoextrato">
+      <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#prepaidcard-saldoextrato" aria-expanded="false" aria-controls="prepaidcard-saldoextrato">
+        <h4 class="panel-title">
+          Como verificar o saldo e extrato?
+        </h4>
+      </a>
+    </div>
+    <div id="prepaidcard-saldoextrato" class="panel-collapse collapse" role="tabpanel" aria-labelledby="prepaid-saldoextrato">
+      <div class="panel-body">
+        <p>Acesse o site <a href="http://www.rendimentovtm.com.br" target="_blank">www.rendimentovtm.com.br</a>, realize o cadastro e efetue a consulta, que também poderá ser realizada no aplicativo móvel Banco Rendimento para celulares e tablets.</p>
+        <p>Se preferir, entre em contato com a Central Visa TravelMoney*, ou também entre em contato com a Focco através de nossos telefones <a href="tel:+551150515299">(11) 5051-5299</a> / <a href="tel:+551138623859">(11) 3862-3859</a> ou e-mail <a href="mailto:contato@foccocambio.com.br">contato@foccocambio.com.br</a>.</p>
+        <p><small>*Utilize os telefones internacionais gratuitos ou ligue a cobrar.</small></p>
+
+      </div>
+    </div>
+  </div>
+
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="prepaid-stolen">
+      <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#prepaidcard-stolen" aria-expanded="false" aria-controls="prepaidcard-stolen">
+        <h4 class="panel-title">
+          Roubo ou perda do cartão
+        </h4>
+      </a>
+    </div>
+    <div id="prepaidcard-stolen" class="panel-collapse collapse" role="tabpanel" aria-labelledby="prepaid-stolen">
+      <div class="panel-body">
+        <p>Entre imediatamente em contato com a Central Visa TravelMoney* e efetue o bloqueio de seu cartão. Você ainda pode bloqueá-lo no site <a href="http://www.rendimentovtm.com.br" target="_blank">www.rendimentovtm.com.br</a> ou pelo aplicativo móvel Banco Rendimento para celulares e tablets.</p>
+        <p><strong>No site:</strong> Cadastre-se no site. Após acessar o site com seu usuário e senha, clique em &quot;Perda/Roubo&quot;, preencha as informações solicitadas e confirme o bloqueio.</p>
+        <p><strong>No aplicativo móvel:</strong> Após acessar o aplicativo com seu usuário e senha, utilize o menu &quot;Bloquear Cartão&quot;, preencha as informações solicitadas e confirme o bloqueio.</p>
+        <p><small>*Até que o bloqueio seja feito, todas as transações efetuadas são de sua responsabilidade. Lembre-se de manter a sua senha separada do cartão, para evitar o uso indevido.</small></p>
+        <p><small>*Utilize os telefones internacionais gratuitos ou ligue a cobrar. </small></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="prepaid-abroad">
+      <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#prepaidcard-abroad" aria-expanded="false" aria-controls="prepaidcard-abroad">
+        <h4 class="panel-title">
+          Pagamento em Reais no exterior
+        </h4>
+      </a>
+    </div>
+    <div id="prepaidcard-abroad" class="panel-collapse collapse" role="tabpanel" aria-labelledby="prepaid-abroad">
+      <div class="panel-body">
+        <p>Alguns estabelecimentos no exterior passaram a oferecer a opção de pagamento em REAIS com cartão. Essa opção é escolhida por você no momento da compra.</p>
+        <p>Ao assinar o comprovante ou digitar a senha, tendo escolhido o pagamento em REAIS, você estará concordando com a taxa de conversão estabelecida pelo próprio estabelecimento.</p>
+        <p>Ao realizar compras em estabelecimentos que solicitam assinatura de contrato, fique atento às condições, pois você poderá estar aceitando efetuar o pagamento em reais
+          descrito em alguma cláusula, sendo assim, para evitar gastos e tarifas indesejadas, pedimos que dê preferência para o pagamento na moeda local do país em que estiver ao utilizar o seu Cartão Visa Travel Money. </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading" role="tab" id="prepaid-cruise">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#prepaidcard-cruise" aria-expanded="false" aria-controls="prepaidcard-cruise">
+          <h4 class="panel-title">
+            Pagamento em cruzeiros marítimos
+          </h4>
+        </a>
+      </div>
+      <div id="prepaidcard-cruise" class="panel-collapse collapse" role="tabpanel" aria-labelledby="prepaid-cruise">
+        <div class="panel-body">
+          <p>A utilização do cartão pode ser restrita em alguns cruzeiros marítimos em função de seus procedimentos internos.</p>
+          <p>Lembre-se de consultar previamente a aceitação do cartão no local da viagem. </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading" role="tab" id="prepaid-tips">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#prepaidcard-tips" aria-expanded="false" aria-controls="prepaidcard-tips">
+          <h4 class="panel-title">
+            Compras com Gorjetas
+          </h4>
+        </a>
+      </div>
+      <div id="prepaidcard-tips" class="panel-collapse collapse" role="tabpanel" aria-labelledby="prepaid-tips">
+        <div class="panel-body">
+          <p>Ao realizar compras com o cartão verifique se o estabelecimento acrescentou a gorjeta (em inglês, "Tip") ao valor da transação. </p>
+          <p>Nunca assine o comprovante sem o preenchimento devido desta cobrança, que é opcional, caso não concorde, inutilize o espaço destinado à gorjeta.</p>
+          <p>Ao assinar o comprovante ou digitar a senha, você estará concordando com este valor extra que será debitado diretamente do saldo do seu cartão. </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading" role="tab" id="prepaid-prebooking">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#prepaidcard-prebooking" aria-expanded="false" aria-controls="prepaidcard-prebooking">
+          <h4 class="panel-title">
+            Posso fazer pré-reserva de hotéis e locadora de veículos?
+          </h4>
+        </a>
+      </div>
+      <div id="prepaidcard-prebooking" class="panel-collapse collapse" role="tabpanel" aria-labelledby="prepaid-prebooking">
+        <div class="panel-body">
+          <p>O seu cartão pode ser utilizado para pré-reserva e pagamento de hotel ou de aluguel de veículos, mas o saldo ficará bloqueado no valor da pré-reserva ou
+           pagamento, acrescido de uma tarifa de 15% de segurança sobre esse valor por um prazo de até 15 dias corridos, até que o estabelecimento confirme a transação.
+         </p>
+         <p>A responsabilidade por essa transação, assim como pelo seu cancelamento, se for o caso, é exclusivamente do estabelecimento, já que este
+           é o único que tem a possibilidade de efetuar lançamentos de débito no cartão.</p>
+         </div>
+       </div>
+     </div>
+
+
+   </div>
--- novo/int_cartao_prepago.php
+++ novo/int_cartao_prepago.php
@@ -5,7 +5,41 @@
     imagem cartão pré-pago
   </div>
 </div>
-<div class="container">
+<div class="container blue-text">
+  <div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+      <h2 class="text-uppercase text-center"><strong>Cartão Visa Travel Money</strong></h2>
+      <p class="text-center">O Cartão Visa Travel Money é um cartão pré-pago, recarregável, que pode ser utilizado para compras e saques em suas viagens internacionais. É a maneira mais moderna e segura de levar dinheiro, já que é carregado com o valor em moeda estrangeira e as compras e saques são realizados na moeda do país em que estiver. </p>
+      <h4>Quais são suas vantagens?</h4>
+      <ul>
+        <li>Sem risco de variação cambial;</li>
+        <li>Rede de aceitação Visa, presente em mais de 200 países e territórios**;</li>
+        <li>Mais de 30 milhões de estabelecimentos credenciados para fazer suas compras;</li>
+        <li>Para sua segurança, podemos gravar o nome no cartão.</li>
+        <li>Saques em mais de 2 milhões de caixas automáticos filiados a rede Plus espalhados pelo mundo; (Tarifa de 2,50 na moeda do cartão é debitada automaticamente do saldo).</li>
+        <li>O cartão está disponível nas moedas dólar americano, euro, libra esterlina, dólar australiano, dólar canadense e peso argentino; e as compras e saques são realizados na moeda local do país em que estiver;</li>
+        <li>Disponibilidade de até 4 (quatro) cartões adicionais que acessam o mesmo saldo;</li>
+        <li>Primeira*** reposição gratuita no local onde você solicitar;</li>
+        <li>Recarga à distância, garantindo mais tranquilidade na sua viagem;</li>
+        <li>Recompra de saldo garantido pelo valor de mercado. Verifique o processo ligando para <a href="tel:+551150515299">(11) 5051-5299</a>;</li>
+        <li>Consultas de saldo ou extrato diretamente no site, ou através do aplicativo gratuito no seu celular.</li>
+      </ul>
+      <p><small>**Nos países que sofrem embargo político e/ou econômico dos EUA, a aceitação do cartão é restrita.</small></p>
+      <p><small>***A partir da segunda reposição no exterior, haverá um custo. A tarifa será debitada diretamente do saldo disponível do cartão.</small></p>
+    </div>
+  </div>
+  <hr>
+  <div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+      <h3 class="text-center text-uppercase">Como utilizar seu cartão pré-pago?</h3>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+<?php include 'comp_prepaid_card.php';?>
+    </div>
+  </div>
+
 </div>
 
 <?php include '_footer.php';?>
