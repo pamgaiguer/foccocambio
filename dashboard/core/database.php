6e8037c910f74148f97e2da958dcb6371a2f0e01
Pam Gaiguer <pamella.gaiguer@gmail.com>
2016-06-10 17:18:58 -0300

    adding database.php

---
+++ dashboard/core/database.php
@@ -0,0 +1,11 @@
+<?php
+
+$dbHost = "localhost";
+$dbUser = "root";
+$dbPwd = "root";
+$dbName = "focco";
+
+$conn = mysqli_connect($dbHost, $dbUser, $dbPwd, $dbName);
+if (!$conn) die('N&atilde;o foi poss&iacute;vel conectar: ' . mysql_error());
+
+?>
