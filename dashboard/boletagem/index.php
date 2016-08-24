3c088f373907049198d766a8c2781173f417095f
Pam Gaiguer <pamella.gaiguer@gmail.com>
2016-08-24 00:58:25 -0300

    adicionando diretório boletagem

---
+++ dashboard/boletagem/index.php
@@ -0,0 +1,24 @@
+<?php
+
+include "../includes/header.php";
+include "../core/database.php";
+
+?>
+<main>
+  <div class="row">
+    <div class="col s12">
+      <h4>Boletagem</h4>
+      <blockquote>
+        Área para boletagem.
+      </blockquote>
+
+      <p>Em desenvolvimento...</p>
+
+    </div>
+  </div>
+</div>
+</main>
+
+<?php
+include '../includes/footer.php';
+?>
