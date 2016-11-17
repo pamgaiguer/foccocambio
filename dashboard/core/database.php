c6c3d3a48eb244b61f85e9ce0cdb474915ccc34e
Pam Gaiguer <pamella.gaiguer@gmail.com>
2016-11-17 14:22:02 -0200

    updating banco de dados

--- dashboard/core/database.php
+++ dashboard/core/database.php
@@ -5,12 +5,10 @@
   $dbPwd = "";
   $dbName = "focco";
 
-
-/*$dbHost = "186.232.189.174";
-$dbUser = "foccocam_admin";
-$dbPwd = "foccoCam2855";
-$dbName = "foccocam_dash";
-*/
+// $dbHost = "localhost";
+// $dbUser = "foccocam_admin";
+// $dbPwd = "foccoCam2855";
+// $dbName = "foccocam_dash";
 
 $conn = mysqli_connect($dbHost, $dbUser, $dbPwd, $dbName);
 if (!$conn) die('N&atilde;o foi poss&iacute;vel conectar: ' . mysql_error());
