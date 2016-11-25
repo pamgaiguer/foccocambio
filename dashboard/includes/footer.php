2b4dbf22893aed1564ed80279616723f688ae1f8
Pam Gaiguer <pamella.gaiguer@gmail.com>
2016-11-25 16:43:14 -0200

    adicionando loader no footer

--- dashboard/includes/footer.php
+++ dashboard/includes/footer.php
@@ -1,16 +1,31 @@
 
-  <script src="/dashboard/scripts/jquery-2.2.4.min.js"></script>
-  <script src="/dashboard/scripts/jquery.mask.min.js"></script>
-  <script src="/dashboard/scripts/materialize.min.js"></script>
-  <script src="/dashboard/scripts/jquery.elevatezoom.js"></script>
-  <script src="/dashboard/scripts/main.js"></script>
-  <script>
-    $(document).ready(function(){
+<div class="preloader-wrapper active">
+<div class="spinner-layer spinner-blue-only">
+    <div class="circle-clipper left">
+      <div class="circle"></div>
+    </div>
+    <div class="gap-patch">
+      <div class="circle"></div>
+    </div>
+    <div class="circle-clipper right">
+      <div class="circle"></div>
+    </div>
+  </div>
+</div>
+
+
+<script src="/dashboard/scripts/jquery-2.2.4.min.js"></script>
+<script src="/dashboard/scripts/jquery.mask.min.js"></script>
+<script src="/dashboard/scripts/materialize.min.js"></script>
+<script src="/dashboard/scripts/jquery.elevatezoom.js"></script>
+<script src="/dashboard/scripts/main.js"></script>
+<script>
+  $(document).ready(function(){
     $('select').material_select();
 
-      focco.sideNav();
-      focco.loginFormPost();
-    });
-  </script>
-  </body>
-  </html>
+    focco.sideNav();
+    focco.loginFormPost();
+  });
+</script>
+</body>
+</html>
