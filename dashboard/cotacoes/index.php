0c2114530f7699673171d7d68baaa34b68be81cd
Pam Gaiguer <pamella.gaiguer@gmail.com>
2016-12-13 23:50:40 -0200

    preparing new cotation table with exchange rates dinamically through api

--- dashboard/cotacoes/index.php
+++ dashboard/cotacoes/index.php
@@ -26,11 +26,8 @@ $libra = $rows[0]['libra'];
     </div>
   </div>
 
-  <div class="row">
+<!--   <div class="row">
     <div class="col s12">
-    <div class="alert warning" style="display: none;">
-        <p>pra q isso pam?</p>
-      </div>
       <div class="alert success" style="display: none;">
         <p>Tabela atualizada com sucesso</p>
       </div>
@@ -38,7 +35,7 @@ $libra = $rows[0]['libra'];
         <p>Erro ao tentar atualizar a tabela</p>
       </div>
     </div>
-  </div>
+  </div> -->
 
   <form id="form-cotacoes">
     <div class="row">
@@ -47,20 +44,38 @@ $libra = $rows[0]['libra'];
         <table class="cotation-table">
           <thead>
             <tr>
-              <th>Dólar</th>
-              <th></th>
-              <th>Euro</th>
-              <th></th>
-              <th>Libra</th>
+              <th class="tooltipped" data-position="top" data-delay="50" data-tooltip="Dólar Americano">USD</th>
+              <th class="tooltipped" data-position="top" data-delay="50" data-tooltip="Euro">EUR</th>
+              <th class="tooltipped" data-position="top" data-delay="50" data-tooltip="Libra">GBP</th>
+              <th class="tooltipped" data-position="top" data-delay="50" data-tooltip="Dólar Australiano">AUD</th>
+              <th class="tooltipped" data-position="top" data-delay="50" data-tooltip="Dólar Canadense">CAD</th>
+              <th class="tooltipped" data-position="top" data-delay="50" data-tooltip="Franco Suíco">CHF</th>
+              <th class="tooltipped" data-position="top" data-delay="50" data-tooltip="Iên">JPY</th>
+              <th class="tooltipped" data-position="top" data-delay="50" data-tooltip="Dólar Neozelandês">NZD</th>
+              <th class="tooltipped" data-position="top" data-delay="50" data-tooltip="Peso Chileno">CLP</th>
+              <th class="tooltipped" data-position="top" data-delay="50" data-tooltip="Peso Mexicano">MXN</th>
+              <th class="tooltipped" data-position="top" data-delay="50" data-tooltip="Peso Uruguaio">UYU</th>
+              <th class="tooltipped" data-position="top" data-delay="50" data-tooltip="Peso Argentino">ARS </th>
+              <th class="tooltipped" data-position="top" data-delay="50" data-tooltip="Rand Sul Africano">ZAR</th>
+              <th class="tooltipped" data-position="top" data-delay="50" data-tooltip="Yuan Chinês">CNY</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td><input type="number" step="0.01" id="input-dolar" value="<?php echo $dolar ?>"></td>
-              <td></td>
-              <td><input type="number" step="0.01" id="input-euro" value="<?php echo $euro ?>"></td>
-              <td></td>
-              <td><input type="number" step="0.01" id="input-libra" value="<?php echo $libra ?>"></td>
+              <td>item1</td>
+              <td>item2</td>
+              <td>item3</td>
+              <td>item4</td>
+              <td>item5</td>
+              <td>item6</td>
+              <td>item7</td>
+              <td>item8</td>
+              <td>item9</td>
+              <td>item10</td>
+              <td>item11</td>
+              <td>item12</td>
+              <td>item13</td>
+              <td>item14</td>
             </tr>
           </tbody>
         </table>
@@ -68,11 +83,6 @@ $libra = $rows[0]['libra'];
       </div>
     </div>
 
-    <div class="row">
-      <div class="col s12">
-        <input type="submit" id="input-submit" value="Atualizar valores" class="btn bg-blue" />
-      </div>
-    </div>
   </form>
 </main>
 
