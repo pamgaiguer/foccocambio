eff9e2b7d644a17ad2eb969aa2afee828789ec19
Pam Gaiguer <pamella.gaiguer@gmail.com>
2016-12-17 00:00:51 -0200

    compra de moedas - tela feita

---
+++ dashboard/compraMoedas/index.php
@@ -0,0 +1,22 @@
+<?php
+
+include "../includes/header.php";
+include "../core/database.php";
+
+?>
+<main>
+  <div class="row">
+    <div class="col s12">
+      <h4>Compra de Moedas</h4>
+      <blockquote>
+        Área para compra de moedas para os caixas da Focco Câmbio
+      </blockquote>
+    </div>
+  </div>
+
+
+</main>
+
+<?php
+include '../includes/footer.php';
+?>
