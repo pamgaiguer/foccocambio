7dac6c73261f11a33fbac16f78491130d41fe906
Pam Gaiguer <pamella.gaiguer@gmail.com>
2017-01-05 01:28:27 -0200

    tabela de orcamento

--- orcamento.php
+++ orcamento.php
@@ -1,5 +1,94 @@
   <?php include 'includes/_header.php'; ?>
 
-orçamento
+  <main class="orcamento">
+    <div class="container">
+      <div class="row bg-blue-2">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <div class="row bg-blue">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+              <img src="assets/images/cotacao-focco-logo.png" class="logo" alt="logo focco">
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+              <p class="link-white">www.foccocambio.com.br</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row bg-gray-table">
+        <div class="col-xs-12 col-sm-12 col-md 12 col-lg-12">
+          <div class="gray-stripe">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>Moeda</th>
+                  <th>Operação</th>
+                  <th>Tipo de operação</th>
+                  <th>Quantidade</th>
+                  <th>Taxa</th>
+                  <th>IOF</th>
+                  <th>Sub Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>usd dólar americano</td>
+                  <td>Compra</td>
+                  <td>Espécie</td>
+                  <td>3.000,00</td>
+                  <td>3,28</td>
+                  <td>5.000,00</td>
+                  <td>15.000,00</td>
+                </tr>
+                <tr>
+                  <td>usd dólar americano</td>
+                  <td>Compra</td>
+                  <td>Espécie</td>
+                  <td>3.000,00</td>
+                  <td>3,28</td>
+                  <td>5.000,00</td>
+                  <td>15.000,00</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div class="row bg-blue-2">
+        <div class="spacing"></div>
+        <div class="spacing"></div>
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4"></div>
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+          <div class="text-center cotacao-resume">
+            <p>Total de iof: <span>R$ 150,00</span></p>
+            <p>Custo de operação: <span>R$ 150,00</span></p>
+            <p>darf: <span>R$ 150,00</span></p>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4"></div>
+      </div>
 
-<?php include 'includes/_footer.php';?>
+      <div class="row bg-blue-2">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <div class="spacing"></div>
+          <hr>
+        </div>
+      </div>
+
+      <div class="row bg-blue-2">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <h1 class="total-vet text-center">Total vet: <span>R$ 15.000,00</span></h1>
+        </div>
+      </div>
+
+      <div class="row bg-dark-blue">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <h3 class="text-center">
+            Valor referencial, favor confirmar a taxa na hora do fechamento.
+          </h3>
+        </div>
+      </div>
+
+    </div>
+  </main>
+
+  <?php include 'includes/_footer.php';?>
