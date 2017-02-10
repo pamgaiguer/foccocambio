f8a7855f0018fe7aa2ca6ec90b1b604c85327d3b
Pam Gaiguer <pamella.gaiguer@gmail.com>
2017-02-09 23:13:54 -0200

    corrigindo problema no sidebar

--- dashboard/includes/header.php
+++ dashboard/includes/header.php
@@ -28,13 +28,8 @@ date_default_timezone_set('America/Sao_Paulo');
     <nav class="bg-gray">
       <!-- Barra do topo! -->
       <ul class="right hide-on-med-and-down">
-<!--         <li><a href="#!"></a></li>
-  <li><a href="#!"></a></li> -->
-  <li><a class="dropdown-button" href="#!" data-activates="dropOptions"><?php echo $_SESSION['currentUser']['nome']  ?><i class="material-icons right">&#xE5C5;</i></a></li>
-  <ul id='dropOptions' class='dropdown-content'>
-<!--           <li><a href="#!">First</a></li>
-          <li><a href="#!">Second</a></li>
-          <li><a href="#!">Third</a></li> -->
+        <li><a class="dropdown-button" href="#!" data-activates="dropOptions"><?php echo $_SESSION['currentUser']['nome']  ?><i class="material-icons right">&#xE5C5;</i></a></li>
+        <ul id='dropOptions' class='dropdown-content'>
           <li class="divider"></li>
           <li><a href="/dashboard/logout.php"><i class="material-icons left">&#xE879;</i> Logoff</a></li>
         </ul>
@@ -46,25 +41,18 @@ date_default_timezone_set('America/Sao_Paulo');
         <li class="brand-logo center"><img class="" src="/dashboard/images/logo_topo_minor.png" alt="logotipo"></li>
         <li><a href="#!"></a></li>
         <li><a href="/dashboard/home"><i class="material-icons left">&#xE871;</i> Início</a></li>
-        <li class="">
-          <ul class="collapsible collapsible-accordion">
-            <li>
-              <a class="collapsible-header"><i class="material-icons left">&#xE3C9;</i> Cadastros<i class="material-icons right">&#xE5C5;</i></a>
-              <div class="collapsible-body">
-                <ul>
-                  <?php
-                  if ($_SESSION['currentUser']['tipo'] < 4)
-                    echo
-                  '<li><a href="/dashboard/usuarios/"><i class="material-icons left">&#xE8D3;</i> Usuários</a></li>';
+        <a class='dropdown-button' href='#' data-activates='cadastros'><i class="material-icons left">&#xE3C9;</i>  Cadastros <i class="material-icons right">&#xE5C5;</i></a>
+        <ul id='cadastros' class='dropdown-content'>
+          <li class="divider"></li>
+          <?php
+          if ($_SESSION['currentUser']['tipo'] < 4)
+            echo
+          '<li><a href="/dashboard/usuarios/"><i class="material-icons left">&#xE8D3;</i> Usuários</a></li>';
 
-                  if ($_SESSION['currentUser']['tipo'] < 5)
-                    echo '<li><a href="/dashboard/clientes/"><i class="material-icons left">&#xE7FE;</i> Clientes</a></li>';
-                  ?>
-                </ul>
-              </div>
-            </li>
-          </ul>
-        </li>
+          if ($_SESSION['currentUser']['tipo'] < 5)
+            echo '<li><a href="/dashboard/clientes/"><i class="material-icons left">&#xE7FE;</i> Clientes</a></li>';
+          ?>
+        </ul>
 
         <?php
         if ($_SESSION['currentUser']['tipo'] < 2)
@@ -73,21 +61,16 @@ date_default_timezone_set('America/Sao_Paulo');
         <li><a href="/dashboard/blog/"><i class="material-icons left">&#xE8CD;</i> Blog do João</a></li>
         ';
         ?>
-
         <?php if ($_SESSION['currentUser']['tipo'] < 4)
         echo
         '<li><a href="/dashboard/cotacoes"><i class="material-icons left">&#xE227;</i> Cotações</a></li>'
         ?>
-
-
         <li><a href="/dashboard/boletagem"><i class="material-icons left">&#xE922;</i> Boletagem</a></li>
         <li><a href="/dashboard/orcamento"><i class="material-icons left">&#xE85D;</i> Orçamento</a></li>
-
         <?php if ($_SESSION['currentUser']['tipo'] < 3)
         echo '<li><a href="/dashboard/compraMoedas"><i class="material-icons left">&#xE8CC;</i> Interbancários</a></li>';
         ?>
         <li><a href="/dashboard/calculadora"><i class="material-icons left">&#xE0BC;</i> Calculadora</a></li>
       </ul>
     </nav>
-
   </header>
