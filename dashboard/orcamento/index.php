a86f3fcf74a633117fb73ad39b07ec89616dd240
Pam Gaiguer <pamella.gaiguer@gmail.com>
2017-02-10 02:19:45 -0200

    orcamento layout corrigido

--- dashboard/orcamento/index.php
+++ dashboard/orcamento/index.php
@@ -17,23 +17,33 @@ include "../core/database.php";
   <div class="row">
     <div class="col s3">
       <div class="input-field">
-      <input  id="nomeCliente" type="text">
+        <input  id="nomeCliente" type="text">
         <label for="nomeCliente">Nome Cliente</label>
       </div>
     </div>
     <div class="col s3">
       <div class="input-field">
-      <input  id="emailCliente" type="text">
+        <input  id="emailCliente" type="text">
         <label for="emailCliente">E-mail do cliente</label>
       </div>
     </div>
   </div>
 
   <div class="row">
-    <div class="col s12"></div>
+    <div class="col s12">
+      <a class="btn bg-blue right" href="#">Adicionar orçamento <i class="material-icons right">shopping_cart</i></a>
+    </div>
   </div>
 
   <div class="row">
+    <div class="col s2">
+      <label for="orcMoedas">Operação</label>
+      <select id="orcMoedas" class="browser-default">
+        <option value="Venda">Venda</option>
+        <option value="Compra">Compra</option>
+      </select>
+    </div>
+
     <div class="col s2">
       <label for="orcMoedas">Moedas</label>
       <select id="orcMoedas" class="browser-default">
@@ -55,10 +65,72 @@ include "../core/database.php";
     </div>
 
     <div class="col s2">
+      <label for="orcOperacao">Tipo de operação</label>
+      <select id="orcOperacao" class="browser-default">
+        <option value="1">Espécie</option>
+        <option value="2">Cartão Pré-Pago</option>
+        <option value="3">Transferência internacional</option>
+        <option value="4">Transferência internacional - Importação (IOF: 0,00)</option>
+      </select>
+    </div>
+
+    <div class="input-field col s1">
+      <input id="orcQtdade" name="orcQtdade" type="text">
+      <label for="orcQtdade">Quantidade</label>
+    </div>
+
+    <div class="input-field col s1">
+      <input id="orcTaxa" name="orcTaxa" type="text">
+      <label for="orcTaxa">Taxa</label>
+    </div>
+
+    <div class="input-field col s1">
+      <input id="orcIOF" name="orcIOF" type="text">
+      <label for="orcIOF">IOF (%)</label>
+    </div>
+
+    <div class="input-field col s1">
+      <input id="orcTotalSIOF" name="orcTotalSIOF" type="text">
+      <label for="orcTotalSIOF">Total s/ IOF</label>
+    </div>
+
+
+    <div class="input-field col s1">
+      <input id="orcDARF" name="orcDARF" type="text">
+      <label for="orcDARF">DARF</label>
+    </div>
+    <div class="input-field col s1">
+      <input id="orcQuantia" name="orcQuantia" type="text">
+      <label for="orcQuantia">VET</label>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col s2">
       <label for="orcMoedas">Operação</label>
       <select id="orcMoedas" class="browser-default">
         <option value="Venda">Venda</option>
         <option value="Compra">Compra</option>
+      </select>
+    </div>
+
+    <div class="col s2">
+      <label for="orcMoedas">Moedas</label>
+      <select id="orcMoedas" class="browser-default">
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="GBP">GBP</option>
+        <option value="AUD">AUD</option>
+        <option value="CAD">CAD</option>
+        <option value="CHF">CHF</option>
+        <option value="JPY">JPY</option>
+        <option value="NZD">NZD</option>
+        <option value="CLP">CLP</option>
+        <option value="MXN">MXN</option>
+        <option value="UYU">UYU</option>
+        <option value="ZAR">ZAR</option>
+        <option value="ARS">ARS</option>
+        <option value="CNY">CNY</option>
       </select>
     </div>
 
@@ -73,6 +145,11 @@ include "../core/database.php";
     </div>
 
     <div class="input-field col s1">
+      <input id="orcQtdade" name="orcQtdade" type="text">
+      <label for="orcQtdade">Quantidade</label>
+    </div>
+
+    <div class="input-field col s1">
       <input id="orcTaxa" name="orcTaxa" type="text">
       <label for="orcTaxa">Taxa</label>
     </div>
@@ -82,22 +159,96 @@ include "../core/database.php";
       <label for="orcIOF">IOF (%)</label>
     </div>
 
-    <div class="input-field col s2">
-      <input id="orcQuantia" name="orcQuantia" type="text">
-      <label for="orcQuantia">Quantia</label>
-    </div>
-
-    <div class="input-field col s2">
+    <div class="input-field col s1">
       <input id="orcTotalSIOF" name="orcTotalSIOF" type="text">
       <label for="orcTotalSIOF">Total s/ IOF</label>
+    </div>
+
+    <div class="input-field col s1">
+      <input id="orcDARF" name="orcDARF" type="text">
+      <label for="orcDARF">DARF</label>
+    </div>
+    <div class="input-field col s1">
+      <input id="orcQuantia" name="orcQuantia" type="text">
+      <label for="orcQuantia">VET</label>
     </div>
   </div>
 
   <div class="row">
+    <div class="col s2">
+      <label for="orcMoedas">Operação</label>
+      <select id="orcMoedas" class="browser-default">
+        <option value="Venda">Venda</option>
+        <option value="Compra">Compra</option>
+      </select>
+    </div>
+
+    <div class="col s2">
+      <label for="orcMoedas">Moedas</label>
+      <select id="orcMoedas" class="browser-default">
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="GBP">GBP</option>
+        <option value="AUD">AUD</option>
+        <option value="CAD">CAD</option>
+        <option value="CHF">CHF</option>
+        <option value="JPY">JPY</option>
+        <option value="NZD">NZD</option>
+        <option value="CLP">CLP</option>
+        <option value="MXN">MXN</option>
+        <option value="UYU">UYU</option>
+        <option value="ZAR">ZAR</option>
+        <option value="ARS">ARS</option>
+        <option value="CNY">CNY</option>
+      </select>
+    </div>
+
+    <div class="col s2">
+      <label for="orcOperacao">Tipo de operação</label>
+      <select id="orcOperacao" class="browser-default">
+        <option value="1">Espécie</option>
+        <option value="2">Cartão Pré-Pago</option>
+        <option value="3">Transferência internacional</option>
+        <option value="4">Transferência internacional - Importação (IOF: 0,00)</option>
+      </select>
+    </div>
+
+    <div class="input-field col s1">
+      <input id="orcQtdade" name="orcQtdade" type="text">
+      <label for="orcQtdade">Quantidade</label>
+    </div>
+
+    <div class="input-field col s1">
+      <input id="orcTaxa" name="orcTaxa" type="text">
+      <label for="orcTaxa">Taxa</label>
+    </div>
+
+    <div class="input-field col s1">
+      <input id="orcIOF" name="orcIOF" type="text">
+      <label for="orcIOF">IOF (%)</label>
+    </div>
+
+    <div class="input-field col s1">
+      <input id="orcTotalSIOF" name="orcTotalSIOF" type="text">
+      <label for="orcTotalSIOF">Total s/ IOF</label>
+    </div>
+
+    <div class="input-field col s1">
+      <input id="orcDARF" name="orcDARF" type="text">
+      <label for="orcDARF">DARF</label>
+    </div>
+    <div class="input-field col s1">
+      <input id="orcQuantia" name="orcQuantia" type="text">
+      <label for="orcQuantia">VET</label>
+    </div>
+  </div>
+
+
+  <div class="row">
     <div class="col s12">
       <button class="btn bg-blue right">
-      Enviar orçamento por e-mail
-<i class="material-icons right">send</i>
+        Enviar orçamento por e-mail
+        <i class="material-icons right">send</i>
       </button>
 
     </div>
